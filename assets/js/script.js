842bd84cebbb08911a6946fb8599c00d1d194808
Sohrab <mdsohrab1431@gmail.com>
2024-02-26 13:23:28 +0600

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * navber bariables
+ */ 
+ 
+
+const navToggleBtn = document.querySelector("[data-nav-toggle-btn]");
+const header = document.querySelector("[data-header]");
+
+
+navToggleBtn.addEventListener("click", function () { header.classList.toggle("active");
+});
